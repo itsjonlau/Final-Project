8b3ac12b3b94d766cb65c944f86573dce15e2710
lmsullivan18 <74469315+lmsullivan18@users.noreply.github.com>
2021-05-02 10:37:35 -0400

    updating

--- Lauren/dev/Database.sql
+++ Lauren/dev/Database.sql
@@ -1,25 +1,23 @@
 -- Creating two tables for final project, bringing in clean data from "cleaning data" file
-CREATE TABLE weekly_deaths (
+CREATE TABLE deaths (
 	Week DATE NOT NULL,
+	HHS_Region VARCHAR(2) NOT NULL,
 	Race VARCHAR(255) NOT NULL,
 	COVID_Deaths INT NOT NULL
 );
 
 CREATE TABLE vaccine_distro (
 	Week DATE NOT NULL,
-	Vaccines INT NOT NULL
+	Vaccinations FLOAT NOT NULL
 );
 
---merge the tables together into the datasets needed for my analysis
--- first table is data for HHS1 only
+--merge the tables together into the dataset needed for my analysis
 SELECT w.Week,
-	w.Race
+	w.Race,
 	w.COVID_Deaths,
-	v.Vaccines
-INTO HHS1
+	v.Vaccinations
+INTO Total_Data
 FROM weekly_deaths as w
 INNER JOIN vaccine_distro as v
 ON (w.Week=v.Week)
 ORDER BY w.Week;
-
---will create tables for each HHS region
