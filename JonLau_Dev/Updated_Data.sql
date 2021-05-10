22cab57f138ba2bbdeec71a5a3910ba963ddc70e
itsjonlau <jonathanlau95@yahoo.com>
2021-05-10 18:20:15 -0400

    updating

--- JonLau_Dev/Updated_Data.sql
+++ JonLau_Dev/Updated_Data.sql
@@ -1,13 +1,1 @@
---Create a new table for weekly data
-Create Table Updated_Data_Week(
-	Week_Ending Date Not Null,
-	Age_Group VARCHAR,
-	COVID_Deaths INT
-);
-
---Create a new table for monthly data
-Create Table Updated_Data(
-	Monthly Date Not null
-	age_group Varchar(0)
-	COVID_Deaths INT
-);
+{"version":"50000","data":{"id":"5705f811-120d-49c9-96c2-5ae9b85ed3f4","offsetX":10.444914806084164,"offsetY":3.2571816263926414,"zoom":96.66666666666666,"gridSize":15,"layers":[{"id":"a71c2f43-753b-42d4-b798-d31e3d841374","type":"diagram-links","isSvg":true,"transformed":true,"models":{}},{"id":"50820910-80d8-4b8e-9a85-f4cac2356739","type":"diagram-nodes","isSvg":false,"transformed":true,"models":{"67142bd6-d972-41a4-8a81-db565f707cf6":{"id":"67142bd6-d972-41a4-8a81-db565f707cf6","type":"table","selected":true,"x":270,"y":15,"ports":[],"name":"Untitled","color":"rgb(0,192,255)","portsInOrder":[],"portsOutOrder":[],"otherInfo":{"data":{"columns":[{"name":"Week of Dist","description":null,"attnum":1,"cltype":"date","is_primary_key":false,"attnotnull":true,"attlen":null,"attprecision":null,"attidentity":"","colconstype":"n","seqincrement":null,"seqstart":null,"seqmin":null,"seqmax":null,"seqcache":null,"seqcycle":null},{"name":"Vax Dist","description":null,"attnum":2,"cltype":"bigint","is_primary_key":false,"attnotnull":true,"attlen":null,"attprecision":null,"attidentity":"","colconstype":"n","seqincrement":null,"seqstart":null,"seqmin":null,"seqmax":null,"seqcache":null,"seqcycle":null}],"name":"CleanVaxdb","schema":"public","description":null,"primary_key":[]},"note":""}},"e52054a0-33b6-4757-87b7-78c3abf6a6e4":{"id":"e52054a0-33b6-4757-87b7-78c3abf6a6e4","type":"table","selected":false,"x":15,"y":15,"ports":[],"name":"Untitled","color":"rgb(0,192,255)","portsInOrder":[],"portsOutOrder":[],"otherInfo":{"data":{"columns":[{"name":"week_ending","description":null,"attnum":1,"cltype":"date","is_primary_key":false,"attnotnull":true,"attlen":null,"attprecision":null,"attidentity":"","colconstype":"n","seqincrement":null,"seqstart":null,"seqmin":null,"seqmax":null,"seqcache":null,"seqcycle":null},{"name":"age_group","description":null,"attnum":2,"cltype":"character varying","is_primary_key":false,"attnotnull":false,"attlen":null,"attprecision":null,"attidentity":"","colconstype":"n","seqincrement":null,"seqstart":null,"seqmin":null,"seqmax":null,"seqcache":null,"seqcycle":null},{"name":"covid_deaths","description":null,"attnum":3,"cltype":"integer","is_primary_key":false,"attnotnull":false,"attlen":null,"attprecision":null,"attidentity":"","colconstype":"n","seqincrement":null,"seqstart":null,"seqmin":null,"seqmax":null,"seqcache":null,"seqcycle":null}],"name":"Updated_Data_Week","schema":"public","description":null,"primary_key":[]},"note":""}}}}]}}
